40840a1b8357940fb18718994d935950447ed18f
Sourabh Dey <98477908+sourabhdey21@users.noreply.github.com>
2023-10-01 21:29:53 +0530

    Create keypair.tf

---
+++ Terraform/aws_3_tier_application/keypair.tf
@@ -0,0 +1,6 @@
+
+# Create keypair for ec2 instance 
+resource "aws_key_pair" "keypair" {
+  key_name   = "access"                              # Change this to your desired key pair name
+  public_key = file("/home/sourabh/.ssh/id_rsa.pub") #Replace with your keypair 
+}
